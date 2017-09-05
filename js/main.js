1c0ce1c774d12940416891098ef0361fc238026f
jessica-liyan <13125157002@163.com>
2017-09-05 14:20:19 +0800

    fix

---
+++ js/main.js
@@ -0,0 +1,13 @@
+function deviceType(){
+  var ua = navigator.userAgent;
+  var agent = ["Android", "iPhone", "SymbianOS", "Windows Phone", "iPad", "iPod"];    
+  for(var i=0; i<len,len = agent.length; i++){
+      if(ua.indexOf(agent[i])>0){        
+          break;
+      }
+  }
+}
+deviceType();
+window.addEventListener('resize', function(){
+  deviceType();
+})
